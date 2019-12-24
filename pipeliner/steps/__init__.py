f0ec30a4feb2a0dee2be2a1c0a0c3d5b0e201e2c
Luboš Matejčík <lubos.matejcik@gmail.com>
2019-12-24 14:08:43 +0100

    Add SSL and TLS support for send email step

--- pipeliner/steps/__init__.py
+++ pipeliner/steps/__init__.py
@@ -3,5 +3,5 @@ from .get_html_element_text import GetHtmlElementText
 from .get_html_element import GetHtmlElement
 from .http_download import HttpDownload
 from .compare_with_previous import CompareWithPrevious
-from .send_email import SendEmail
+from .send_email import SendEmailSsl, SendEmailTls
 from .do_nothing import DoNothing
--- pipeliner/steps/send_email.py
+++ pipeliner/steps/send_email.py
@@ -1,5 +1,6 @@
 import logging
 import smtplib
+import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -8,7 +9,40 @@ from pipeliner.steps.step import Step
 logger = logging.getLogger(__name__)
 
 
-class SendEmail(Step):
+class SendEmailTls(Step):
+    def __init__(self, smtp_host: str, smtp_port: int, login: str, password: str, from_email: str, to_email: str, subject: str):
+        super().__init__()
+        self._smtp_host = smtp_host
+        self._smtp_port = smtp_port
+        self._login = login
+        self._password = password
+        self._from_email = from_email
+        self._to_email = to_email
+        self._subject = subject
+
+    def perform(self, data: str) -> str:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+        with smtplib.SMTP(self._smtp_host, self._smtp_port) as server:
+            server.ehlo()
+            server.starttls(context=context)
+            server.ehlo()
+            server.login(self._login, self._password)
+
+            message = MIMEMultipart("alternative")
+            message["Subject"] = self._subject
+            message["From"] = self._from_email
+            message["To"] = self._to_email
+
+            message.attach(MIMEText(data, "plain"))
+            message.attach(MIMEText(data, "html"))
+
+            server.sendmail(self._from_email, self._to_email, message.as_string())
+
+        logger.info("Email was sent successfully")
+        return data
+
+
+class SendEmailSsl(Step):
     def __init__(self, smtp_host: str, smtp_port: int, login: str, password: str, from_email: str, to_email: str, subject: str):
         super().__init__()
         self._smtp_host = smtp_host
